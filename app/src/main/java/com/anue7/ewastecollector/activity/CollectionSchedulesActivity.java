4de634987215a7172b904b1a522b2108cabad5dc
mohnish <mohnishmb@gmail.com>
2014-11-08 18:02:10 +0530

    Adding Location Service code.

--- app/src/main/java/com/anue7/ewastecollector/activity/CollectionSchedulesActivity.java
+++ app/src/main/java/com/anue7/ewastecollector/activity/CollectionSchedulesActivity.java
@@ -1,19 +1,62 @@
 package com.anue7.ewastecollector.activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.anue7.ewastecollector.R;
 
-public class CollectionSchedulesActivity extends Activity {
+public class CollectionSchedulesActivity extends Activity implements LocationListener {
+
+    private TextView longitude;
+
+    private TextView latitude;
+
+    private LocationManager locationManager;
+
+    private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_collection_schedules);
+        setLocation();
 	}
+
+    private void setLocation(){
+        longitude = (TextView) findViewById(R.id.textView2);
+        latitude = (TextView) findViewById(R.id.textView4);
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if(provider != null) {
+            Location location = locationManager.getLastKnownLocation(provider);
+
+            // Initialize the location fields
+            if (location != null) {
+                System.out.println("Provider " + provider + " has been selected.");
+                onLocationChanged(location);
+            } else {
+                latitude.setText("Location not available");
+                longitude.setText("Location not available");
+            }
+        }
+        else{
+            latitude.setText("Provider not available");
+            longitude.setText("Provider not available");
+        }
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,4 +76,29 @@ public class CollectionSchedulesActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        latitude.setText(String.valueOf(lat));
+        longitude.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
 }
